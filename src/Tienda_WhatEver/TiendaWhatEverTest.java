8d5510756a8ddf61595310b234bb69d1ce005177
Adolfo <Adolfo@Adolfo_Chavez>
2026-04-28 19:55:18 -0600

    se declaro la clase Test de la TiendaWhatEver

---
+++ src/Tienda_WhatEver/TiendaWhatEverTest.java
@@ -0,0 +1,9 @@
+
+package Tienda_WhatEver;
+
+public class TiendaWhatEverTest {
+    public static void main(String[] args) {
+        TiendaWhatEver tienda = new TiendaWhatEver();
+        tienda.app();
+    }
+}
